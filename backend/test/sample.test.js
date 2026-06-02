1f01e19557501607d2dd4661e0648f7989333208
rushanagune <rgunewardene@icloud.com>
2026-06-02 21:19:15 +1000

    test: Add sample test file for CI pipeline

---
+++ backend/test/sample.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('Sample Tests', function() {
+  it('should pass this test', function() {
+    assert.equal(1, 1);
+  });
+
+  it('should also pass', function() {
+    assert.strictEqual(true, true);
+  });
+});
